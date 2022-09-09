595d0de8597c2cd58d56f765de7bdfa562152cbc
ahmedkhairym0 <ahmedkhairym0@gmail.com>
2022-09-10 01:47:44 +0200

    feat:Make search for course feature

--- src/app/routes/home/components/coursesSection.js
+++ src/app/routes/home/components/coursesSection.js
@@ -5,23 +5,29 @@ import styles from "./coursesSection.module.css";
 
 function CoursesSection({ courses }) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const search = searchParams.get("search");
+  const search = searchParams.get("search") || "";
 
   useEffect(() => {
-    const params = searchParams.get("search");
+    // const params = searchParams.get("search");
 
-    if (params === "") {
+    if (search === "") {
       searchParams.delete("search");
       setSearchParams(searchParams);
     }
-    console.log('params', params);
+    console.log("params", search);
   }, [search]);
 
+  const coursesArr = courses.filter((course) =>
+    course.title.toLowerCase().includes(search.toLowerCase())
+  );
 
-  const coursesArr = courses.filter((course) => (
-    <CourseCard course={course} key={course.id} />
-  ));
-  return <div className={styles.coursesCardsGrid}>{coursesArr}</div>;
+  return (
+    <div className={styles.coursesCardsGrid}>
+      {coursesArr.map((course) => (
+        <CourseCard course={course} key={course.id} />
+      ))}
+    </div>
+  );
 }
 
 export default CoursesSection;
